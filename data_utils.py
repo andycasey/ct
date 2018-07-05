aa2cf7913bff5b074a87657dcd7eca44f54d3bba
Andy Casey <andycasey@gmail.com>
2018-07-05 15:21:11 +1000

    lots of mlf clustering intuition

--- data_utils.py
+++ data_utils.py
@@ -9,6 +9,8 @@ def generate_data(N, D, J, K=1, seed=None, full_output=False, **kwargs):
     :math:`K` clusters.
     """
 
+    assert D == 10 and J == 3, \
+        "Sorry these are fixed until we generate latent factors randomly"
     if seed is not None:
         np.random.seed(seed)
 
@@ -16,7 +18,8 @@ def generate_data(N, D, J, K=1, seed=None, full_output=False, **kwargs):
     mu_epsilon = np.zeros(D)
 
     phi = np.eye(J)
-    psi = np.diag(np.abs(np.random.normal(0, 1, size=D)))
+    psi_scale = kwargs.get("__psi_scale", 1)
+    psi = np.diag(np.abs(np.random.normal(0, 1, size=D))) * psi_scale
 
     # TODO: generate latent factors randomly... but keep near orthogonality
     L = np.array([
@@ -34,15 +37,20 @@ def generate_data(N, D, J, K=1, seed=None, full_output=False, **kwargs):
         p = np.abs(np.random.normal(0, 1, K))
         responsibility = np.random.choice(np.arange(K), N, p=p/p.sum())
 
-        cluster_scale = kwargs.get("cluster_scale", 1)
         cluster_mu_theta = np.random.multivariate_normal(
-            mu_theta, cluster_scale * np.abs(np.random.normal(0, 1, size=J)) * np.eye(J),
-            size=K)
-        cluster_mu_sigma = np.abs(
-            np.random.multivariate_normal(np.zeros(J), np.eye(J), size=K))
+            np.zeros(J), np.eye(J), size=K)
+
+        #S = 1 if kwargs.get("__cluster_common_scale", True) else J
+        #cluster_mu_sigma = np.abs(np.random.multivariate_normal(
+        #    np.zeros(J), 
+        #    cluster_scale * np.random.normal(0, 1, size=S) * np.eye(J), 
+        #    size=K))
+
+        scale = kwargs.get("__cluster_scale", 1)
+        cluster_sigma_theta = scale * np.abs(np.random.normal(0, 1, size=K))
 
         theta = np.zeros((N, J), dtype=float)
-        for k, (mu, cov) in enumerate(zip(cluster_mu_theta, cluster_mu_sigma)):
+        for k, (mu, cov) in enumerate(zip(cluster_mu_theta, cluster_sigma_theta)):
             members = (responsibility == k)
             theta[members] = np.random.multivariate_normal(mu, np.eye(J) * cov, 
                                                            size=sum(members))
@@ -52,7 +60,8 @@ def generate_data(N, D, J, K=1, seed=None, full_output=False, **kwargs):
 
     y = np.dot(theta, L) + epsilon
     data = dict(J=J, N=N, D=D, K=K, y=y)
-    truths = dict(L=L, psi=psi, epsilon=epsilon, theta=theta)
+    truths = dict(L=L, psi=psi, epsilon=epsilon, theta=theta, 
+                  responsibility=responsibility)
 
     return (data, truths) if full_output else data
 
